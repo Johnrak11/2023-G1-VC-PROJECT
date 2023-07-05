d181a1dc7592accd06036fe827ab87c2b3cb59a7
CHAMRETH.KOSAL <CHAMRETH.KOSAL@PNC.LAN>
2023-07-05 16:21:12 +0700

    [VRG1-26] confirm password

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-<<<<<<< HEAD
 import RegisterForm from '../views/RegisterForm.vue'
 
 const routes = [
@@ -8,24 +7,22 @@ const routes = [
     name: 'register',
     component: RegisterForm
   },
-=======
-
-const routes = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView
-  // },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
->>>>>>> login
 ]
+// const routes = [
+//   // {
+//   //   path: '/',
+//   //   name: 'home',
+//   //   component: HomeView
+//   // },
+//   {
+//     path: '/about',
+//     name: 'about',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+//   }
+// ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
