ec8fb5743917dde2b25abc77a2073522dd930fad
nit123456 <nit.noem.@student.passerellesnumeriques.org>
2023-07-16 15:29:06 +0700

    [VRG1-63] correct table name

--- backend/database/migrations/2023_07_04_011409_create_credit__cards_table.php
+++ backend/database/migrations/2023_07_04_011409_create_credit__cards_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('creditcards', function (Blueprint $table) {
+        Schema::create('credit__cards', function (Blueprint $table) {
             $table->id();
             $table->string('name',);
             $table->integer('cvv');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('creditcards');
+        Schema::dropIfExists('credit__cards');
     }
 };
