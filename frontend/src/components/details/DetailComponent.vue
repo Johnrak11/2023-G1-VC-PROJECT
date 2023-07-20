3eea9c0e55f3a48eb63df93a6eab9381a5f9d711
johnrak11 <vorak.yun@student.passerellesnumeriques.org>
2023-07-20 10:16:41 +0700

    [VRG1-153] merging

--- frontend/src/components/details/DetailComponent.vue
+++ frontend/src/components/details/DetailComponent.vue
@@ -1,23 +1,19 @@
 <template>
-  <img
-    class="img1 mt-10"
-    src="https://edgefieldconcerts.com/wp-content/uploads/2022/07/rex-orange-county-2022-ecotl.jpg"
-    alt=""
-  />
+  <img class="img1 mt-10" src="https://edgefieldconcerts.com/wp-content/uploads/2022/07/rex-orange-county-2022-ecotl.jpg"
+    alt="" />
+
+  <v-btn icon class="bg-red back-btn">
+    <v-icon>mdi-arrow-left</v-icon>
+  </v-btn>
   <div class="container d-flex flex-column">
     <div class="detail">
-      <div class="row bg-white d-flex pa-6 flex-column" style="margin-top: -8%">
+      <div class="row bg-white d-flex pa-6 flex-column" style="margin-top: -5%">
         <div id="row1" class="col-md-6">
           <img v-if="event" class="img" :src="event.image" alt="Image" />
           <div class="d-flex mt-2 justify-space-between">
             <div class="icon-left d-flex">
               <div class="d-flex">
-                <v-icon
-                  class="mt-1"
-                  :color="liked ? 'grey' : 'red'"
-                  @click="liked = !liked"
-                  >mdi-heart</v-icon
-                >
+                <v-icon class="mt-1" :color="liked ? 'grey' : 'red'" @click="liked = !liked">mdi-heart</v-icon>
                 <p class="mt-1 ml-2">100</p>
               </div>
               <div class="d-flex ml-10">
@@ -28,7 +24,7 @@
             <div class="icon-right">
               <div class="d-flex">
                 <v-icon class="mr-2" icon="mdi-ticket" size="30"></v-icon>
-                <h3 class="tic mt-1 ml-2">300</h3>
+                <h3 class="tic mt-1 ml-2" v-if="eventDetail">{{ eventDetail.available_ticket }}</h3>
               </div>
             </div>
           </div>
@@ -40,7 +36,7 @@
             {{ event.description }}
           </p>
           <div class="contact d-flex justify-space-between">
-            <div class="icons mt-3">
+            <div class="icons mt-3 d-flex flex-column justify-space-between">
               <div class="d-flex">
                 <v-icon class="icon1"> mdi-calendar</v-icon>
                 <p class="ml-4" v-if="event">{{ event.date }}</p>
@@ -51,40 +47,28 @@
               </div>
               <div class="d-flex g-icon">
                 <v-icon class="icon1">mdi-map-marker</v-icon>
-                <p class="ml-4" v-if="event">{{ event.location }}</p>
+                <p class="ml-4 location-text" v-if="event">{{ event.location }}</p>
               </div>
-              <div class="d-flex g-icon">
+              <div class="d-flex g-icon align-center">
                 <v-icon class="icon1" size="26">mdi-cash</v-icon>
-                <h2 class="ml-3 price " v-if="eventDetail && eventDetail.price">
+                <h2 class="ml-3 text-red" v-if="eventDetail && eventDetail.price">
                   {{ eventDetail.price }}
                 </h2>
-                <h3 class="ml-3 price mt-1" v-else>Free</h3>
               </div>
               <br />
             </div>
             <div class="map">
-              <iframe
+              <ShowMap v-if="event" :eventInfor="event" ></ShowMap>
+              <!-- <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12345.678901234567!2d-122.4324!3d37.7897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMznCsDU4JzU0LjQiTiAxMjLCsDM1JzQ5LjIiVw!5e0!3m2!1sen!2sus!4v1625706808123!5m2!1sen!2sus"
-                width="455"
-                height="200"
-                style="border: 0"
-                allowfullscreen=""
-                loading="lazy"
-              ></iframe>
+                width="455" height="200" style="border: 0" allowfullscreen="" loading="lazy"></iframe> -->
             </div>
           </div>
           <div class="d-flex justify-end button mt-6">
-            <button
-              class="free bg-red pa-1 rounded"
-              v-if="!eventDetail || !eventDetail.price"
-            >
+            <button class="free bg-red pa-1 rounded" v-if="!eventDetail || !eventDetail.price">
               Register
             </button>
-            <button
-              class="free bg-red pa-1 rounded"
-              @click.prevent="booking(event.id)"
-              v-else
-            >
+            <button class="free bg-red pa-1 rounded" @click.prevent="booking(event.id)" v-else>
               Booking
             </button>
           </div>
@@ -95,67 +79,59 @@
     <listCard v-if="event" :event="event"></listCard>
   </div>
 </template>
-<script>
+<script setup>
 import router from "@/routes/router";
 import { ref, onMounted } from "vue";
 import agendaComponent from "./AgendaComponent.vue";
-import listCardDetail from "./ListCardDetail.vue";
+import ShowMap from "../maps/ShowMap.vue";
+import listCard from "./ListCardDetail.vue";
 import baseAPI from "@/stores/axiosHandle.js";
 import { useRoute } from "vue-router";
-export default {
-  components: {
-    agendaComponent,
-    listCard: listCardDetail,
-  },
-  setup() {
-    const liked = ref(false);
-    const event = ref(null);
-    const eventDetail = ref(null);
 
-    const fetchEvent = async () => {
-      const route = useRoute();
-      const eventId = route.params.id;
-      await baseAPI
-        .get(`/events/${eventId}`)
-        .then((response) => {
-          event.value = response.data.data;
-        })
-        .catch((error) => console.log(error));
-    };
+const liked = ref(false);
+const event = ref(null);
+const eventDetail = ref(null);
 
-    const fetchEventDetail = async () => {
-      const route = useRoute();
-      const eventId = route.params.id;
-      await baseAPI
-        .get(`/eventDetail/${eventId}`)
-        .then((response) => {
-          eventDetail.value = response.data.data;
-        })
-        .catch((error) => console.log(error));
-    };
+const fetchEvent = async () => {
+  const route = useRoute();
+  const eventId = route.params.id;
+  try {
+    const response = await baseAPI.get(`/events/${eventId}`);
+    event.value = response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    onMounted(() => {
-      fetchEvent();
-      fetchEventDetail();
-    });
+const fetchEventDetail = async () => {
+  const route = useRoute();
+  const eventId = route.params.id;
+  try {
+    const response = await baseAPI.get(`/eventDetail/${eventId}`);
+    eventDetail.value = response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    return {
-      liked,
-      event,
-      eventDetail,
-    };
-  },
-  methods: {
-    booking(id) {
-      router.push("/booking/" + id);
-    },
-  },
+onMounted(() => {
+  fetchEvent();
+  fetchEventDetail();
+});
+
+const booking = (id) => {
+  router.push("/booking/" + id);
 };
 </script>
 <style scoped>
 .container {
   margin-top: -15%;
   margin-bottom: 30px;
+}
+
+.back-btn {
+  position: absolute;
+  top: 5%;
 }
 
 .detail {
@@ -175,7 +151,16 @@ export default {
   object-fit: cover;
 }
 
+.location-text {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 500px;
+  /* Adjust the max-width as needed */
+}
+
 .img {
+  position: relative;
   width: 100%;
   height: 55vh;
   object-fit: cover;
@@ -186,21 +171,18 @@ h1 {
   font-weight: bold;
   margin-bottom: 20px;
 }
+
 .tic {
   color: red;
 }
+
 .g-icon {
   margin-top: -8%;
 }
 
-.price {
-  margin-top: -3%;
-  color: red;
-}
 
 .detail p {
   font-size: 18px;
   line-height: 1.5;
   margin-bottom: 20px;
-}
-</style>
+}</style>
---
+++ frontend/src/components/maps/ShowMap.vue
@@ -0,0 +1,32 @@
+<template>
+    <div style="height:250px; width:400px" v-if="eventinfor">
+        <l-map ref="map" v-model:zoom="zoom" :center="[eventinfor.latitude, eventinfor.longitude]">
+            <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
+                name="OpenStreetMap"></l-tile-layer>
+            <l-marker :lat-lng="[eventinfor.latitude, eventinfor.longitude]">
+                <l-popup>
+                    <cardTemplate :event="eventinfor"></cardTemplate>
+                </l-popup>
+            </l-marker>
+
+        </l-map>
+    </div>
+</template>
+  
+<script setup>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
+import { ref, defineProps } from "vue";
+import cardTemplate from '../partials/cards/CardTemplate.vue'
+
+
+const zoom = ref(10)
+
+const props = defineProps({
+    eventInfor: Object
+})
+
+const eventinfor = ref(props.eventInfor)
+</script>
+  
+<style></style>
