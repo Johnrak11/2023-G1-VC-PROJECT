1c1aa8f1e4e957962eeb0c01d08919dca1172c3c
THOEUN.SIENGDAV <THOEUN.SIENGDAV@PNC.LAN>
2023-07-26 19:40:18 +0700

    [VRG1-103] update view deleting dashboard

--- frontend/src/stores/eventsStore.js
+++ frontend/src/stores/eventsStore.js
@@ -7,7 +7,6 @@ export const eventStores = defineStore("event", {
     recommendEvent: [],
     recommendEventLimit: [],
     reletedEvent: [],
-    deleteEventById: [],
     localHttp: "http://172.16.0.143:8080",
     pagination: { currentPage: 1, lastPage: 5, links: [], totalPage: 1 },
   }),
@@ -67,7 +66,7 @@ export const eventStores = defineStore("event", {
           let responseData = response.data;
           this.recommendEvent = responseData;
           if (responseData.length > 8) {
-            console.log(responseData.length);
+            console.log(responseData.length)
             this.recommendEventLimit = responseData.slice(0, 8);
           } else {
             this.recommendEventLimit = responseData;
--- frontend/src/views/dashboard/ContainLeftDashboard.vue
+++ frontend/src/views/dashboard/ContainLeftDashboard.vue
@@ -1,8 +1,22 @@
 <template>
-  <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false" :elevation="5">
-    <v-list-item prepend-avatar="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" title="Vanda SOPHAL" nav>
+  <v-navigation-drawer
+    v-model="drawer"
+    :rail="rail"
+    permanent
+    @click="rail = false"
+    :elevation="5"
+  >
+    <v-list-item
+      prepend-avatar="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+      title="Vanda SOPHAL"
+      nav
+    >
       <template v-slot:append>
-        <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+        <v-btn
+          variant="text"
+          icon="mdi-chevron-left"
+          @click.stop="rail = !rail"
+        ></v-btn>
       </template>
     </v-list-item>
     <hr class="w-100" />
@@ -10,18 +24,46 @@
 
     <v-list density="compact">
       <router-link to="/dashboard" class="nav">
-        <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-home-city"
+          title="Home"
+          value="home"
+        ></v-list-item>
       </router-link>
       <router-link to="/dashboard/event" class="nav">
-        <v-list-item prepend-icon="mdi-calendar" title="Event" value="event"></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-calendar"
+          title="Event"
+          value="event"
+        ></v-list-item>
       </router-link>
       <router-link to="/dashboard/preview" class="nav">
-        <v-list-item prepend-icon="mdi-calendar" title="Preview" value="preview"></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-calendar"
+          title="Preview"
+          value="preview"
+        ></v-list-item>
       </router-link>
-      <v-list-item prepend-icon="mdi-help" title="Report" value="report"></v-list-item>
-      <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
-      <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
-      <v-list-item prepend-icon="mdi-information" title="About" value="about"></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-help"
+        title="Report"
+        value="report"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-account"
+        title="My Account"
+        value="account"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-account-group-outline"
+        title="Users"
+        value="users"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-information"
+        title="About"
+        value="about"
+      ></v-list-item>
     </v-list>
   </v-navigation-drawer>
   <NavbarDashboard></NavbarDashboard>
@@ -37,8 +79,8 @@ export default {
     };
   },
   components: {
-    NavbarDashboard
-  }
+    NavbarDashboard,
+  },
 };
 </script>
 <style scoped>
